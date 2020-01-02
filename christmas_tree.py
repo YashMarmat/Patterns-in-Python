5294ff562f9739960135a33be597044f3e6ae3e9
Yash Marmat <59337853+YashMarmat@users.noreply.github.com>
2020-01-02 16:15:31 +0530

    Create christmas_tree.py

---
+++ christmas_tree.py
@@ -0,0 +1,15 @@
+# A christmas tree (just a bigger tree)
+
+n = 7
+for each in range(1, n):
+    print(' ' * (n-each) + each * '* ')
+for each in range(4, n+1):
+    print(' ' * (n-each) + each * '* ')
+for each in range(4, n+1):
+    print(' ' * (n-each) + each * '* ')
+p = 4
+for each in range(1, p):   # running this loop twice
+    print(' ' * 5 + '* *')
+
+
+
